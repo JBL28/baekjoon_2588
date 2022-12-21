350f48f0ce213e7ee9147ff38d927e8291889c2a
JBL28 <rabid28@naver.com>
2022-12-21 21:26:01 +0900

    ver1.0
    1

---
+++ Project/Project/소스.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("%d\n", a * (b % 10));
+	printf("%d\n", a * (b % 100 - b % 10) / 10);
+	printf("%d\n", a * ((b - b % 100) / 100));
+	printf("%d", a * b);
+
+	return 0;
+}
